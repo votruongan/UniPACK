102069e2c193055c18ef1f3fb82a2e3835e6a2fd
An Vo <votrngan@hotmail.com>
2019-04-27 01:02:16 +0700

    demoReady

--- js/UIControl_Book.js
+++ js/UIControl_Book.js
@@ -3,17 +3,59 @@ var bookUpForm = document.getElementsByClassName('modal')[0];
 var bookUpBtn = document.getElementById('submitBookUp');
 var bookUp = document.getElementById('bookUp');
 
+var zoomContent = document.getElementsByClassName('modal')[1];	
+
+$( document ).ready(function() {
+  // Handler for .ready() called.
+	$("#bookUp").hide();
+	$("#notiHolder").hide();
+	$("#showMe").hide();
+
+});
+
+$("#findBtn").click(function(){
+	$("#showMe").show();
+
+});
+
+$("#submitBookUp").click(function(){
+	bookUpForm.style.display = "none";
+});
+
+
+$("#submitBookUp2").click(function(){
+	$("#formHolder").hide(200);
+	$("#notiHolder").show(200);
+});
 
 function showBookUpForm(){
   bookUpForm.style.display = "block";
+  zoomOutContent(100);
 }
 
 function closeBookUpForm(){
 	bookUpForm.style.display = "none";
 }
 
+$("#okButton").click(function(){
+	$("#formHolder").show();
+	$("#notiHolder").hide();
+	bookUpForm.style.display = "none";
+	setTimeout(function(){
+  		$("#bookUp").show();
+	}, 3000);		
+})
+
 bookUpBtn.onclick = function(){
-	bookUpForm.style.display = "none";	
-	bookUp.style.display = "block";
+	$("#formHolder").hide("200");
+	$("#notiHolder").show("200");
 }
-bookUp.style.display = "none";
+
+function zoomInContent(sId){
+	zoomContent.style.display = "block";
+}
+
+
+function zoomOutContent(){
+	zoomContent.style.display = "none";
+}
